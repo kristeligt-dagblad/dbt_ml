f9d149a36ca6bf0fe871ef341cc387e90a6fd324
Ben Liyanage <ben.liyanage@vida.com>
2022-12-14 15:32:15 -0800

    remove _

--- macros/hooks/startup.sql
+++ macros/hooks/startup.sql
@@ -1,4 +1,4 @@
-{% macro _startup() %}
+{% macro startup() %}
 
 {% do adapter.create_schema(api.Relation.create(var('dbt_ml:audit_database'), var('dbt_ml:audit_schema'))) %}
 {{ dbt_ml.create_model_audit_table() }}
