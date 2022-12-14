c1b4f2416ccfc1de3f61db9288881a0d49cecd6f
Ben Liyanage <ben.liyanage@vida.com>
2022-12-14 15:37:12 -0800

    remove startup function

--- macros/hooks/startup.sql
+++
@@ -1,6 +0,0 @@
-{% macro startup() %}
-
-{% do adapter.create_schema(api.Relation.create(var('dbt_ml:audit_database'), var('dbt_ml:audit_schema'))) %}
-{{ dbt_ml.create_model_audit_table() }}
-
-{% endmacro %}
